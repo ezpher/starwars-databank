2411544f743ddad24892618517e52ce634e13272
ezpher <40519064+ezpher@users.noreply.github.com>
2021-06-12 21:32:15 +0800

    make sure to export the material modules in sw-material-module

--- src/app/shared/star-wars-material/star-wars-material.module.ts
+++ src/app/shared/star-wars-material/star-wars-material.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule, MatToolbarModule, MatMenuModule, MatButtonModule, MatInputModule, MatSelectModule, MatCardModule, MatFormFieldModule, MatPaginator, MatSpinner } from '@angular/material';
+import { MatIconModule, MatToolbarModule, MatMenuModule, MatButtonModule, MatInputModule, MatSelectModule, MatCardModule, MatFormFieldModule, MatProgressSpinnerModule, MatPaginatorModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
+const MODULES = [
+  MatToolbarModule, MatIconModule, MatMenuModule, MatButtonModule,
+  MatInputModule, MatSelectModule, MatCardModule,
+  MatFormFieldModule, MatPaginatorModule, MatProgressSpinnerModule, BrowserAnimationsModule
+]
+
 @NgModule({
-  declarations: [
-    MatToolbarModule, MatIconModule, MatMenuModule, MatButtonModule,
-    MatInputModule, MatSelectModule, MatCardModule,
-    MatFormFieldModule, MatPaginator, MatSpinner, BrowserAnimationsModule
-  ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, ...MODULES] ,
+  exports: MODULES
 })
 export class StarWarsMaterialModule { }
